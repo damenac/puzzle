2a868109876b6d6a5c7545f57e65723fa8fff42c
David Mendez-Acuna <damenac@gmail.com>
2015-11-25 00:15:51 +0100

    paper

--- plugins/fr.inria.diverse.puzzle.metrics/test/fr/inria/diverse/puzzle/metrics/Histograms.java
+++ plugins/fr.inria.diverse.puzzle.metrics/test/fr/inria/diverse/puzzle/metrics/Histograms.java
@@ -53,6 +53,7 @@ public class Histograms {
 	// Test cases
 	// ------------------------------------------------
 
+	@Ignore
 	@Test
 	public void computeHistogramCount() throws Exception{
 		ConceptComparison conceptComparisonOperator = new DeepConceptComparison();
@@ -121,68 +122,40 @@ public class Histograms {
 		}
 	}	
 	
-	@Ignore
+//	@Ignore
 	@Test
-	public void computeHistograms() throws Exception{
-		ConceptComparison conceptComparisonOperator = new NamingConceptComparison();
+	public void question2() throws Exception{
+		ConceptComparison conceptComparisonOperator = new DeepConceptComparison();
 
 		SyntacticNamingVennDiagram metrics = new SyntacticNamingVennDiagram();
 		int[][] theMatrix = metrics.getCommonalitiesMatrix(languages, conceptComparisonOperator);
-		Hashtable<Integer, Integer> histogramByConstructs = metrics.computeConstructsCommonality(theMatrix);
 		
-		int max = -1;
+		ArrayList<Double> mmaverages = new ArrayList<Double>();
+		
 		for (int i = 0; i < theMatrix.length; i++) {
-			int current = histogramByConstructs.get(i);
-			System.out.println(i + " - " + histogramByConstructs.get(i));
-			if(current > max)
-				max = current;
-		}
-		
-		System.out.println("max: " + max);
-		
-		ArrayList<Integer> histogram = new ArrayList<Integer>();
-		
-		Iterator<Integer> it = histogramByConstructs.keySet().iterator();
-		int count = 0;
-		while (it.hasNext()) {
-			Integer currentKey = (Integer) it.next();
-			Integer currentValue = histogramByConstructs.get(currentKey);
-			if(currentValue == 0){
-				count ++;
-			}
-		}
-		histogram.add(new Integer(count));
-		
-		int maxI = 0;
-		for (int i = 1; i <= max; i+=100) {
-			it = histogramByConstructs.keySet().iterator();
-			count = 0;
-			while (it.hasNext()) {
-				Integer currentKey = (Integer) it.next();
-				Integer currentValue = histogramByConstructs.get(currentKey);
-				int proxI = i + 100;
-				if(currentValue >= i && currentValue < proxI){
+			int sum = 0;
+			int count = 0;
+			for (int j = 0; j < theMatrix[0].length; j++) {
+				if(theMatrix[i][j] > 0){
+					sum += theMatrix[i][j];
 					count ++;
 				}
 			}
-			histogram.add(new Integer(count));
-			maxI = i;
-		}
-		
-		it = histogramByConstructs.keySet().iterator();
-		count = 0;
-		while (it.hasNext()) {
-			Integer currentKey = (Integer) it.next();
-			Integer currentValue = histogramByConstructs.get(currentKey);
-			if(currentValue >= maxI && currentValue <= max){
-				count ++;
+			
+			if(count != 0){
+				double avg = ((double)sum)/((double)count);
+				mmaverages.add(avg);
+//				System.out.println("adding average: " + avg);
 			}
 		}
-		histogram.add(new Integer(count));
 		
-		
-		for (int i = 0; i < histogram.size(); i++) {
-			System.out.println(i*100 + "," + histogram.get(i));
+		double sumAVG = 0;
+		for (Double _double : mmaverages) {
+			sumAVG += _double;
 		}
+		
+		double AVG = sumAVG/((double)mmaverages.size());
+		System.out.println("AVG: " + AVG);
+		
 	}	
 }
