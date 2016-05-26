39f04ec1475cf189dde5365a6589da694e7c8c2a
David Mendez-Acuna <damenac@gmail.com>
2016-05-26 18:42:51 +0200

    preparing demo on modularization

---
+++ examples/modularized-state-machines/fr.inria.diverse.puzzle.examples.triggers.syntax/src/triggers/impl/TriggerImpl.java
@@ -0,0 +1,73 @@
+/**
+ */
+package triggers.impl;
+
+import java.lang.reflect.InvocationTargetException;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import triggers.Trigger;
+import triggers.TriggersPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Trigger</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class TriggerImpl extends MinimalEObjectImpl.Container implements Trigger {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TriggerImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return TriggersPackage.Literals.TRIGGER;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void evalTrigger(Map<?, ?> context) {
+		// TODO: implement this method
+		// Ensure that you remove @generated or mark it @generated NOT
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+		switch (operationID) {
+			case TriggersPackage.TRIGGER___EVAL_TRIGGER__MAP:
+				evalTrigger((Map<?, ?>)arguments.get(0));
+				return null;
+		}
+		return super.eInvoke(operationID, arguments);
+	}
+
+} //TriggerImpl
