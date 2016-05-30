8122a0443ed45f4e7faeb9f991fd317c5131501c
David Mendez-Acuna <damenac@gmail.com>
2016-05-30 10:54:08 +0200

    preparing demo on modularization. towards hfsm

--- examples/modularized-state-machines/fr.inria.diverse.puzzle.examples/xtend-gen/interpreters/Interpreter.java
+++ examples/modularized-state-machines/fr.inria.diverse.puzzle.examples/xtend-gen/interpreters/Interpreter.java
@@ -1,38 +1,21 @@
 package interpreters;
 
-import CompleteDSL.StateMachineAspect;
-import CompleteDSLPckg.CompleteDSLPckgPackage;
-import CompleteDSLPckg.StateMachine;
-import java.util.Map;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-
 @SuppressWarnings("all")
 public class Interpreter {
   public Interpreter() {
   }
   
-  public void eval(final String modelPath) {
-    XMIResourceFactoryImpl fact = new XMIResourceFactoryImpl();
-    boolean _containsKey = EPackage.Registry.INSTANCE.containsKey(CompleteDSLPckgPackage.eNS_URI);
-    boolean _not = (!_containsKey);
-    if (_not) {
-      EPackage.Registry.INSTANCE.put(CompleteDSLPckgPackage.eNS_URI, CompleteDSLPckgPackage.eINSTANCE);
-    }
-    Map<String, Object> _extensionToFactoryMap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
-    _extensionToFactoryMap.put("*", fact);
-    ResourceSetImpl rs = new ResourceSetImpl();
-    URI uri = URI.createFileURI(modelPath);
-    Resource res = rs.getResource(uri, true);
-    EList<EObject> _contents = res.getContents();
-    EObject _get = _contents.get(0);
-    StateMachine stateMachine = ((StateMachine) _get);
-    StateMachineAspect.eval(stateMachine);
+  public Object eval(final String modelPath) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nStateMachine cannot be resolved to a type."
+      + "\nStateMachine cannot be resolved to a type."
+      + "\nThe method or field CompleteDSLPckgPackage is undefined"
+      + "\nThe method or field CompleteDSLPckgPackage is undefined"
+      + "\nThe method or field CompleteDSLPckgPackage is undefined"
+      + "\neNS_URI cannot be resolved"
+      + "\neNS_URI cannot be resolved"
+      + "\neINSTANCE cannot be resolved"
+      + "\neval cannot be resolved");
   }
   
   public static void main(final String[] args) {
